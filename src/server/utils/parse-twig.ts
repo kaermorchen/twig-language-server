fda01201144a7194299016423e901ee75b4957e1
Stanislav Romanov <kaermorchen@gmail.com>
2026-03-23 18:46:43 +0300

    Refactored parseTwig

--- src/server/utils/parse-twig.ts
+++ src/server/utils/parse-twig.ts
@@ -3,20 +3,27 @@ import { join } from 'path';
 import Parser from 'web-tree-sitter';
 import { serverConfig } from '../server';
 
-let parser: Parser;
+let parser: Promise<Parser>;
 
 export async function parseTwig(content: string): Promise<Parser.Tree> {
   if (!parser) {
-    await Parser.init();
-    parser = new Parser();
-    parser.setLanguage(
-      await Parser.Language.load(
-        await readFile(
+    parser = new Promise(async (resolve, reject) => {
+      try {
+        await Parser.init();
+        const parser = new Parser();
+        const wasm = await readFile(
           join(serverConfig.extensionPath, 'out', 'tree-sitter-twig.wasm'),
-        ),
-      ),
-    );
+        );
+        const lang = await Parser.Language.load(wasm);
+
+        parser.setLanguage(lang);
+
+        resolve(parser);
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
-  return parser.parse(content);
+  return parser.then((parser) => parser.parse(content));
 }
